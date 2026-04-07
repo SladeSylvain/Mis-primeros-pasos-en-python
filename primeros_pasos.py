afa8b097f84f62c818eec66719c87e47d51d1f6f
id:001 <alexcarvajal@outlook.fr>
2026-04-07 15:50:53 -0400

    mis primeros pasos en python

---
+++ primeros_pasos.py
@@ -0,0 +1,22 @@
+import math
+
+print("Hola, Este es mi primer programa")
+
+
+nombre = "Alexander"
+edad = 25
+ciudad = "Santiago"
+
+print(f"mi nombre es {nombre } vivo en {ciudad} y tengo {edad}")
+
+numero1 = 5
+numero2 = 10
+
+total = numero1 + numero2
+print(f"la suma de {numero1 } mas {numero2 } es {total}")
+
+
+numeroRaiz = 25 
+raizCuadrada = math.sqrt(numeroRaiz)
+
+print(f"La raiz cuadrada de {numeroRaiz } es: {raizCuadrada}")
